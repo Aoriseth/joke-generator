51f09af5276af779176f17767a1a0de7de72c97b
Lennart Cockx <lennart.cockx@gmail.com>
2018-12-27 09:27:42 +0100

    Switched back to preferred annotation style configuration

--- src/main/java/me/cockx/learn/jokegenerator/config/JokeConfiguration.java
+++ src/main/java/me/cockx/learn/jokegenerator/config/JokeConfiguration.java
@@ -3,8 +3,9 @@ package me.cockx.learn.jokegenerator.config;
 
 import guru.springframework.norris.chuck.ChuckNorrisQuotes;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
-//@Configuration
+@Configuration
 public class JokeConfiguration {
 
     @Bean
