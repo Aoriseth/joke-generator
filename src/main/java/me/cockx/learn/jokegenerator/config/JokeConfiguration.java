422b4b20e55f6f9614c234a21ff60a01b3042ee3
Lennart Cockx <lennart.cockx@gmail.com>
2018-12-27 09:26:27 +0100

    Demo of xml based configuration

--- src/main/java/me/cockx/learn/jokegenerator/config/JokeConfiguration.java
+++ src/main/java/me/cockx/learn/jokegenerator/config/JokeConfiguration.java
@@ -3,9 +3,8 @@ package me.cockx.learn.jokegenerator.config;
 
 import guru.springframework.norris.chuck.ChuckNorrisQuotes;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
+//@Configuration
 public class JokeConfiguration {
 
     @Bean
--- src/main/java/me/cockx/learn/jokegenerator/services/JokeServiceImpl.java
+++ src/main/java/me/cockx/learn/jokegenerator/services/JokeServiceImpl.java
@@ -1,9 +1,11 @@
 package me.cockx.learn.jokegenerator.services;
 
 import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.context.annotation.ImportResource;
 import org.springframework.stereotype.Service;
 
 @Service
+@ImportResource("classpath:joke-config.xml")
 public class JokeServiceImpl implements JokeService {
 
     private final ChuckNorrisQuotes chuckNorrisQuotes;
